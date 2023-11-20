ba30a536f00c607e003d0844f490858fc8c8df34
irriz-jayy <jayfelix789@gmail.com>
2023-11-20 22:31:41 +0300

    added colors to tailwind config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f9b72b",
+        secondary: "#190d01",
+      },
+    },
   },
   plugins: [],
 };
