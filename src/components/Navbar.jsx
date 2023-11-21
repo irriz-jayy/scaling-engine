393492d59f62864e15ef5a641ea62692892dc05f
irriz-jayy <jayfelix789@gmail.com>
2023-11-21 11:52:32 +0300

    added initial styling and links to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <div className="h-[10vh] border bg-secondary text-primary">navbar</div>
+      <nav className="h-[10vh] border bg-secondary text-primary flex justify-between items-center">
+        <Link to="/">Chutney</Link>
+        <ul className="flex">
+          <li className="p-2 mx-2 rounded-md">
+            <NavLink
+              to="/orders"
+              exact
+              className="p-2 transition-colors duration-300 ease-in-out rounded-md hover:bg-primary hover:text-secondary focus:outline-none focus:ring focus:border-blue-300"
+              activeClassName="bg-primary text-secondary"
+            >
+              Order
+            </NavLink>
+          </li>
+          <li className="p-2 mx-2 rounded-md">
+            <NavLink
+              to="/maps"
+              className="p-2 transition-colors duration-300 ease-in-out rounded-md hover:bg-primary hover:text-secondary focus:outline-none focus:ring focus:border-blue-300"
+              activeClassName="bg-primary text-secondary "
+            >
+              Maps
+            </NavLink>
+          </li>
+          <li className="p-2 mx-2 rounded-md">
+            <Link
+              to="/contact"
+              className="p-2 transition-colors duration-300 ease-in-out rounded-md hover:bg-primary hover:text-secondary focus:outline-none focus:ring focus:border-blue-300"
+              activeClassName="bg-primary text-secondary "
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import Navbar from "../components/Navbar";
 
 const Maps = () => {
   const markers = [
@@ -16,20 +17,23 @@ const Maps = () => {
     { geocode: [0.521, 35.2653], popUp: "Chutney bites, Highlands Mall" },
   ];
   return (
-    <MapContainer center={[0.5176, 35.2781]} zoom={14} className="h-60 w-96">
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* Allows grouping of markers when zoomed out to avoid crowding */}
-      <MarkerClusterGroup chunkedLoading>
-        {markers.map((marker) => (
-          <Marker position={marker.geocode}>
-            <Popup>{marker.popUp}</Popup>
-          </Marker>
-        ))}
-      </MarkerClusterGroup>
-    </MapContainer>
+    <>
+      <Navbar />
+      <MapContainer center={[0.5176, 35.2781]} zoom={14} className="h-60 w-96">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Allows grouping of markers when zoomed out to avoid crowding */}
+        <MarkerClusterGroup chunkedLoading>
+          {markers.map((marker) => (
+            <Marker position={marker.geocode}>
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
+    </>
   );
 };
 
