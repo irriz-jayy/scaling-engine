0e60eea28ad7289476fe435f514de861f0597c28
irriz-jayy <jayfelix789@gmail.com>
2023-11-21 22:19:58 +0300

    added responsiveness to the hero section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,9 +5,10 @@ export const Homepage = () => {
   return (
     <>
       <div className="min-h-screen bg-primary">
-        <div className="flex border h-[80vh]">
-          <div className="flex flex-col items-center justify-center w-1/2 border">
-            <p className="p-2 text-6xl border font-main">
+        {/* hero section */}
+        <div className="flex min-h-[80vh] md:h-[90vh] lg:h-[90vh] flex-col md:flex-row lg:flex-row">
+          <div className="flex flex-col items-center justify-center lg:flex-col h-1/2 md:h-full lg:h-full md:w-1/2 lg:w-1/2">
+            <p className="p-2 text-6xl font-main">
               Welcome to the home of flavour
             </p>
             <p className="p-2 font-paragraph">
@@ -21,7 +22,11 @@ export const Homepage = () => {
               Order Now 🍔
             </button>
           </div>
-          <img src={logo} alt="chutney taco" className="" />
+          <img
+            src={logo}
+            alt="chutney taco"
+            className="object-contain w-full h-1/2 md:h-full lg:h-full md:w-1/2 lg:w-1/2"
+          />
         </div>
       </div>
     </>
