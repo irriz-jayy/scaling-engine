17817a9711b7bf45a57d7e7d587d2f3a4e80ffa5
irriz-jayy <jayfelix789@gmail.com>
2023-11-22 15:01:13 +0300

    added contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <div className="min-h-screen bg-primary">Contact</div>;
+};
+
+export default Contact;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import MenuPreview from "../components/MenuPreview";
+import Contact from "../components/Contact";
 
 export const Homepage = () => {
   return (
@@ -8,6 +9,7 @@ export const Homepage = () => {
       <div className="min-h-screen bg-primary">
         <Hero />
         <MenuPreview />
+        <Contact />
       </div>
     </>
   );
