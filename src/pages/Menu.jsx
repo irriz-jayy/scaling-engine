98563cc4166a61c12dab77faa67a3fdf476bb449
irriz-jayy <jayfelix789@gmail.com>
2023-11-22 12:48:44 +0300

    added menu section. Made category bar to sticky. Reduced categories

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import chicken from "../assets/food assets/chicken.jpg";
+import chover from "../assets/food assets/chickenhover.jpg";
+import taco from "../assets/food assets/taco.jpg";
+import thover from "../assets/food assets/tacohover.jpg";
+import fries from "../assets/food assets/fries.jpg";
+import fhover from "../assets/food assets/frieshover.jpg";
+import MealCard from "../components/MealCard";
 
 const Menu = () => {
   const categories = [
@@ -8,28 +15,117 @@ const Menu = () => {
     {
       name: "burgers",
     },
-    {
-      name: "soft drinks",
-    },
+
     {
       name: "tacos",
     },
     {
       name: "chicken",
     },
-    {
-      name: "chicken wings",
-    },
+
     {
       name: "pizza",
+    },
+  ];
+  const food = [
+    {
+      images: {
+        default: chicken,
+        hover: chover,
+      },
+      name: "Fried chicken",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "320 ksh",
+      buttonText: "Order 🍗",
+    },
+    {
+      images: {
+        default: taco,
+        hover: thover,
+      },
+      name: "Beef tacos",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "250 ksh",
+      buttonText: "Order 🌮",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
+    },
+    {
+      images: {
+        default: fries,
+        hover: fhover,
+      },
+      name: "Fries",
+      text: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, excepturi.",
+      price: "200 ksh",
+      buttonText: "Order 🍟",
     },
   ];
   return (
     <>
       <div className="min-h-screen bg-secondary">
-        <div className="fixed flex flex-col w-full border bg-primary">
+        {/* category section */}
+        <div className="sticky z-10 flex flex-col w-full border top-14 bg-primary ">
           <header className="text-lg font-main">Categories</header>
-          <div className="flex">
+          <div className="flex justify-between">
             {categories.map((category, index) => (
               <button
                 key={index}
@@ -39,6 +135,12 @@ const Menu = () => {
               </button>
             ))}
           </div>
+        </div>
+        {/* menu section */}
+        <div className="flex flex-col flex-wrap items-center justify-center min-h-screen gap-4 p-2 md:flex-row">
+          {food.map((item, index) => (
+            <MealCard key={index} data={item} />
+          ))}
         </div>
       </div>
     </>
