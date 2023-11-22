6ebc510988864c213e66a132acc1b5319cd5c4a3
irriz-jayy <jayfelix789@gmail.com>
2023-11-22 15:37:06 +0300

    added icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="h-screen bg-primary">
-      <header className="p-2 text-4xl text-center font-main">
+    <div className="h-screen bg-secondary">
+      <header className="p-2 text-4xl text-center font-main text-primary">
         <span className="underline">Contact</span> us
-        <p className="text-lg text-center text-secondary font-paragraph">
+        <p className="text-lg text-center text-primary font-paragraph">
           Feel free to reach out to us for any concern
         </p>
       </header>
