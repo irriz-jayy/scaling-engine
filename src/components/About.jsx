054384d02cc04911434753b83f5aecdf6d107738
irriz-jayy <jayfelix789@gmail.com>
2023-11-23 16:51:18 +0300

    restructured the code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,38 +8,49 @@ import kids from "../assets/kids.jpg";
 import inclusivity from "../assets/inclusivity.jpg";
 
 const About = () => {
-  const newsItems = [
+  const sections = [
     {
-      image: news,
-      title: "News & Notifications",
-      text: "Follow us, like us, read about us.",
+      id: "values",
+      title: "Our Values",
+      text: "We believe in using our influence, size, and reach, and engaging with customers, staff, Franchisees, suppliers, and partners, as a responsible business with a positive impact on communities globally. We’re proud of the work we do to help make a difference.",
+      items: [
+        {
+          image: energy,
+          title: "Our Planet",
+          text: "Building climate resiliency and protecting nature.",
+        },
+        {
+          image: kids,
+          title: "Food Quality & Sourcing",
+          text: "Creating a future of secure and sustainable food that has a positive impact.",
+        },
+        {
+          image: inclusivity,
+          title: "Jobs, Inclusion & Empowerment",
+          text: "Helping build bright futures through education, training, and accelerating equity.",
+        },
+      ],
     },
     {
-      image: employee,
-      title: "Franchise Info",
-      text: "Learn about Owner/Operator opportunities.",
-    },
-    {
-      image: community,
-      title: "Community Connection",
-      text: "Supporting people when and where they need it most.",
-    },
-  ];
-  const values = [
-    {
-      image: energy,
-      title: "Our Planet",
-      text: "Building climate resiliency and protecting nature.",
-    },
-    {
-      image: kids,
-      title: "Food Quality & Sourcing",
-      text: "Creating a future of secure and sustainable food that has a positive impact.",
-    },
-    {
-      image: inclusivity,
-      title: "Jobs, Inclusion & Empowerment",
-      text: "Helping build bright futures through education, training and accelerating equity.",
+      id: "news",
+      title: "Latest Chutney News",
+      items: [
+        {
+          image: news,
+          title: "News & Notifications",
+          text: "Follow us, like us, read about us.",
+        },
+        {
+          image: employee,
+          title: "Franchise Info",
+          text: "Learn about Owner/Operator opportunities.",
+        },
+        {
+          image: community,
+          title: "Community Connection",
+          text: "Supporting people when and where they need it most.",
+        },
+      ],
     },
   ];
   return (
@@ -67,69 +78,43 @@ const About = () => {
           className="object-contain w-full h-1/2 md:h-full lg:h-full md:w-1/2 lg:w-1/2"
         />
       </div>
-      {/* values section */}
-      <div className="bg-primary text-secondary" id="news">
-        <header className="p-2 text-2xl font-main">
-          Our <span className="underline">values</span>
-          <p className="text-base font-paragraph">
-            We believe in using our influence, size and reach, and engaging with
-            customers, staff, Franchisees, suppliers and partners, as a
-            responsible business with a positive impact on communities globally.
-            We’re proud of the work we do to help make a difference—and will
-            continue working to deliver progress in the communities in which we
-            operate. We’re driving impact by living our purpose. The actions we
-            continue to take today across people, communities and our planet
-            will ensure we’re building a better business and a more trusted
-            brand for generations to come.
-          </p>
-        </header>
-        {/* card section */}
-        <div className="flex flex-col items-center justify-center gap-2 p-2 md:flex-row md:justify-between">
-          {/* cards */}
-          {values.map((value, index) => (
-            <div className="w-80" key={index}>
-              <img
-                className="object-cover w-full h-80"
-                src={value.image}
-                alt=""
-              />
-              <div className="p-2 h-30">
-                <p className="text-lg font-main">{value.title}</p>
-                <p className="text-sm font-paragraph">{value.text}</p>
-                <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
-                  read more
-                </button>
-              </div>{" "}
-            </div>
-          ))}
+      {sections.map((section) => (
+        <div
+          key={section.id}
+          className={`bg-primary text-secondary ${
+            section.id !== "overview" ? "overflow-hidden" : ""
+          }`}
+          id={section.id}
+        >
+          <header className="p-2 text-2xl font-main">
+            {section.title}
+            {section.text && (
+              <p className="text-base font-paragraph">{section.text}</p>
+            )}
+          </header>
+          {/* card section */}
+          <div className="flex flex-col items-center justify-center gap-2 p-2 md:flex-row md:justify-between">
+            {/* cards */}
+            {section.items &&
+              section.items.map((item, index) => (
+                <div className="w-80" key={index}>
+                  <img
+                    className="object-cover w-full h-80"
+                    src={item.image}
+                    alt=""
+                  />
+                  <div className="p-2 h-30">
+                    <p className="text-lg font-main">{item.title}</p>
+                    <p className="text-sm font-paragraph">{item.text}</p>
+                    <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
+                      Read more
+                    </button>
+                  </div>{" "}
+                </div>
+              ))}
+          </div>
         </div>
-      </div>
-      {/* news section */}
-      <div className="bg-primary text-secondary" id="news">
-        <header className="p-2 text-2xl font-main">
-          Latest chutney <span className="underline">news</span>
-        </header>
-        {/* card section */}
-        <div className="flex flex-col items-center justify-center gap-2 p-2 md:flex-row md:justify-between">
-          {/* cards */}
-          {newsItems.map((newsItem, index) => (
-            <div className="w-80" key={index}>
-              <img
-                className="object-cover w-full h-80"
-                src={newsItem.image}
-                alt=""
-              />
-              <div className="p-2 h-30">
-                <p className="text-lg font-main">{newsItem.title}</p>
-                <p className="text-sm font-paragraph">{newsItem.text}</p>
-                <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
-                  read more
-                </button>
-              </div>{" "}
-            </div>
-          ))}
-        </div>
-      </div>
+      ))}
     </>
   );
 };
