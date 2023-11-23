b7416417f5038671f9832de718496bb32217db65
irriz-jayy <jayfelix789@gmail.com>
2023-11-23 22:14:27 +0300

    added text to the contact page

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -9,11 +9,21 @@ const ContactSection = () => {
       <div>
         <header className="p-4 text-4xl font-main">Contact us</header>
         <p className="p-4 text-lg font-paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
+          Looking for nutrition information, employment details or want to
+          contact us? Our team is happy to help you find answers.{" "}
         </p>
       </div>
       {/* FAQs section */}
       <FAQs />
+      {/* feedback section */}
+      <div>
+        <p className="p-4 text-4xl font-main">Feedback</p>
+        <p className="p-4 text-base font-paragraph">
+          Whether you have questions, feedback or comments about something you
+          love, we want to know. Tell us about your experience with the
+          restaurant or the app, or reach out for customer support.
+        </p>
+      </div>
     </div>
   );
 };
