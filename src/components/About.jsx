3e7dc7e722c3d3c1dcab122449e3b195c66a28aa
irriz-jayy <jayfelix789@gmail.com>
2023-11-23 16:19:40 +0300

    added dynamic data and rendered

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import aboutimg from "../assets/aboutmain.png";
 import news from "../assets/news.jpg";
+import employee from "../assets/employee.jpg";
+import community from "../assets/community.jpg";
 
 const About = () => {
+  const newsItems = [
+    {
+      image: news,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    },
+    {
+      image: employee,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    },
+    {
+      image: community,
+      title: "Lorem, ipsum dolor.",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    },
+  ];
   return (
     <>
       <div className="flex min-h-[80vh] md:h-[80vh] lg:h-[80vh] flex-col md:flex-row lg:flex-row bg-primary">
@@ -28,49 +47,29 @@ const About = () => {
           className="object-contain w-full h-1/2 md:h-full lg:h-full md:w-1/2 lg:w-1/2"
         />
       </div>
-      <div className="bg-primary text-secondary">
+      <div className="bg-primary text-secondary" id="news">
         <header className="p-2 text-2xl font-main">
           Latest chutney <span className="underline">news</span>
         </header>
         {/* card section */}
-        <div className="flex flex-col items-center justify-center gap-2 p-2 border-2 md:flex-row md:justify-between">
+        <div className="flex flex-col items-center justify-center gap-2 p-2 md:flex-row md:justify-between">
           {/* cards */}
-          <div className="w-80">
-            <img className="object-cover w-full h-80" src={news} alt="" />
-            <div className="p-2 h-30">
-              <p className="text-lg font-main">Lorem, ipsum dolor.</p>
-              <p className="text-sm font-paragraph">
-                Lorem ipsum, dolor sit amet consectetur adipisicing.
-              </p>
-              <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
-                read more
-              </button>
+          {newsItems.map((newsItem, index) => (
+            <div className="w-80" key={index}>
+              <img
+                className="object-cover w-full h-80"
+                src={newsItem.image}
+                alt=""
+              />
+              <div className="p-2 h-30">
+                <p className="text-lg font-main">{newsItem.title}</p>
+                <p className="text-sm font-paragraph">{newsItem.text}</p>
+                <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
+                  read more
+                </button>
+              </div>{" "}
             </div>
-          </div>
-          <div className="w-80">
-            <img className="object-cover w-full h-80" src={news} alt="" />
-            <div className="p-2 h-30">
-              <p className="text-lg font-main">Lorem, ipsum dolor.</p>
-              <p className="text-sm font-paragraph">
-                Lorem ipsum, dolor sit amet consectetur adipisicing.
-              </p>
-              <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
-                read more
-              </button>
-            </div>
-          </div>
-          <div className="w-80">
-            <img className="object-cover w-full h-80" src={news} alt="" />
-            <div className="p-2 h-30">
-              <p className="text-lg font-main">Lorem, ipsum dolor.</p>
-              <p className="text-sm font-paragraph">
-                Lorem ipsum, dolor sit amet consectetur adipisicing.
-              </p>
-              <button className="w-48 h-12 p-2 my-2 border-4 rounded-md border-primary hover:border-secondary bg-secondary text-primary hover:bg-primary hover:text-secondary font-main">
-                read more
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
