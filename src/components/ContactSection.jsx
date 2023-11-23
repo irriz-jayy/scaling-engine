1a5f1c5bc1d4fb2991dbaae56549e90f6873a907
irriz-jayy <jayfelix789@gmail.com>
2023-11-23 22:05:50 +0300

    added faqs section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import FAQs from "./FAQs";
 
 const ContactSection = () => {
-  return <div>ContactSection</div>;
+  return (
+    // whole page div
+    <div className="min-h-screen bg-primary text-secondary">
+      {/* header */}
+      <div>
+        <header className="p-4 text-4xl font-main">Contact us</header>
+        <p className="p-4 text-lg font-paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
+      </div>
+      {/* FAQs section */}
+      <FAQs />
+    </div>
+  );
 };
 
 export default ContactSection;
