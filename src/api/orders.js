ee0afd67d025cc0845b9ef1d4834f1dc616eb288
irriz-jayy <jayfelix789@gmail.com>
2024-01-05 10:32:04 +0300

    updated the order url and renderd orders appropriately

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,6 +1,6 @@
 // order.js
 
-const API_URL = "http://localhost:3001/orders";
+const API_URL = "http://localhost:3000/api/v1/orders";
 
 // Function to fetch orders
 export const fetchOrders = async () => {
